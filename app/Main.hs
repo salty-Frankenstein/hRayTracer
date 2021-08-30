e47d43e03e43b2e10c311911f0f39b4544a309f4
salty-Frankenstein <1328955419@qq.com>
2021-08-30 21:32:16 +0800

    reader

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,9 @@
 module Main where
 
 import Camera
+import qualified Common as C
 import Control.Concurrent
-import Control.Monad.Writer
+import Control.Monad.Reader
 import Data.IORef
 import qualified Data.Vector.Unboxed as U
 import qualified Hitable as HB
@@ -48,40 +49,43 @@ randomScene = do
   push $ sphere (V3.vec3 (-4) 1 0) 1 (lambertian (V3.vec3 0.4 0.2 0.1))
   push $ sphere (V3.vec3 4 1 0) 1 (metal (V3.vec3 0.7 0.6 0.5) 0)
   hitableList <$> readIORef resRef
-  
-color :: R.Ray -> RURef R -> RURef (R, R, R) -> HB.Hitable -> Int -> IO V3.Vec3
-color r rRef ruRef world depth
-  | res = do
+
+color :: R.Ray -> Int -> C.App V3.Vec3
+color r depth = do
+  C.Env ruRef rRef _ world _ <- ask
+  let (res, hrec@(HB.HitRec t p normal material)) = HB.runHit world r 0.001 1000000
+  if res then do
       if depth < 50 then do
-        scattered <- newIORef r -- TODO
-        attenuation <- newIORef (V3.v 0)
-        sres <- HB.runMaterial material r hrec attenuation scattered rRef ruRef
+        scattered <- liftIO $ newIORef r -- TODO
+        attenuation <- liftIO $ newIORef (V3.v 0)
+        sres <- liftIO $ HB.runMaterial material r hrec attenuation scattered rRef ruRef
         if sres then do
-          a <- readIORef attenuation
-          s <- readIORef scattered
-          (a *) <$> color s rRef ruRef world (depth+1)
+          a <- liftIO $ readIORef attenuation
+          s <- liftIO $ readIORef scattered
+          (a *) <$> color s (depth+1)
         else return $ V3.vec3 0 0 0
       else return $ V3.vec3 0 0 0
-  | otherwise = return $ V3.v (1 - t2) * V3.vec3 1 1 1 + V3.v t2 * V3.vec3 0.5 0.7 1.0
+  else return $ V3.v (1 - t2) * V3.vec3 1 1 1 + V3.v t2 * V3.vec3 0.5 0.7 1.0
   where
-    (res, hrec@(HB.HitRec t p normal material)) = HB.runHit world r 0.001 1000000
     unitDir = normalize (R.direction r)
     t2 = 0.5 * (V3.y unitDir + 1)
 
-colorAt :: Camera -> HB.Hitable -> Int -> Int -> RURef R -> RURef (R, R, R) -> IO V3.Vec3
-colorAt cam world i j rRef ruRef = do
-  colRef <- newIORef (V3.vec3 0 0 0)
+colorAt :: Int -> Int -> C.App V3.Vec3
+colorAt i j = do
+  C.Env ruRef rRef cam world _ <- ask
+  colRef <- liftIO $ newIORef (V3.vec3 0 0 0)
   colorAt' colRef
-  readIORef colRef
+  liftIO $ readIORef colRef
   where
     colorAt' colRef = do
-      r <- randPair ns
+      cam <- asks C.camera
+      r <- liftIO $ randPair ns
       U.forM_ r $ \(di, dj) -> do
         let u = (fromIntegral i + di) / fromIntegral nx
             v = (fromIntegral j + dj) / fromIntegral ny
-        r <- getRay cam u v
-        c <- color r rRef ruRef world 0
-        modifyIORef colRef (+ c)
+        r <- liftIO $ getRay cam u v
+        c <- color r 0
+        liftIO $ modifyIORef colRef (+ c)
 
 main :: IO ()
 main = do
@@ -97,13 +101,15 @@ main = do
       aperture = 0.1
   cam <- camera lookfrom lookat (V3.vec3 0 1 0) 20 aspect aperture distToFocus
   world <- randomScene
-  forM_ [ny -1, ny -2 .. 0] $ \j -> do
-    print j
-    forM_ [0 .. nx -1] $ \i -> do
-      col <- (/ fromIntegral ns) <$> colorAt cam world i j rRef ruRef
-      let col' = V3.vec3 (sqrt $ V3.x col) (sqrt $ V3.y col) (sqrt $ V3.z col)
-      let ir = truncate (255.99 * V3.x col')
-          ig = truncate (255.99 * V3.y col')
-          ib = truncate (255.99 * V3.z col')
-      hPutStr fh $ show ir ++ " " ++ show ig ++ " " ++ show ib ++ "\n"
+  runReaderT 
+    (forM_ [ny -1, ny -2 .. 0] $ \j -> do
+       liftIO $ print j
+       forM_ [0 .. nx -1] $ \i -> do
+         col <- (/ fromIntegral ns) <$> colorAt i j 
+         let col' = V3.vec3 (sqrt $ V3.x col) (sqrt $ V3.y col) (sqrt $ V3.z col)
+         let ir = truncate (255.99 * V3.x col')
+             ig = truncate (255.99 * V3.y col')
+             ib = truncate (255.99 * V3.z col')
+         liftIO $ hPutStr fh $ show ir ++ " " ++ show ig ++ " " ++ show ib ++ "\n") 
+    (C.Env ruRef rRef cam world fh)
   hClose fh
